20cec690b944534a80d7e88d77f94bc867549376
lokesh-6 <kadlokesh@gmail.com>
2024-07-16 16:01:08 +0530

    Utils Operations

--- src/index.js
+++ src/index.js
@@ -7,9 +7,14 @@ dotenv.config({
 
 //Second Approach
 connectDB()
-
-
-
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running on ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO DB connection failed ",err)
+})
 
 
 // First Approch
